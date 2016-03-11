c08dc9d084c9a4ecf92d09ae52e82c5646dcb060
Charlie-Ng <kcng@ucdavis.edu>
2016-03-11 03:56:28 -0800

    added leftNav title

--- components/app.jsx
+++ components/app.jsx
@@ -6,6 +6,20 @@ import TagFaces from 'material-ui/lib/svg-icons/image/tag-faces';
 import LibraryBooks from 'material-ui/lib/svg-icons/av/library-books';
 import Dvr from 'material-ui/lib/svg-icons/device/dvr';
 import ErrorOutline from 'material-ui/lib/svg-icons/alert/error-outline';
+import Paper from 'material-ui/lib/paper';
+
+const style = {
+    leftNav: {
+        backgroundColor: '#BBDEFB'
+    },
+    paper: {
+        textAlign: 'center',
+        paddingTop: '24',
+        paddingBottom: '24',
+        fontSize: '24',
+        backgroundColor: '#BBDEFB'
+    }
+};
 
 export default class LeftNavUndockedExample extends React.Component {
 
@@ -16,10 +30,8 @@ export default class LeftNavUndockedExample extends React.Component {
         this.handleClose = this.handleClose.bind(this);
     }
 
-    //handleToggle = () => this.setState({open: !this.state.open});
     handleToggle() {this.setState({open: !this.state.open});};
 
-    //handleClose = () => this.setState({open: false});
     handleClose() {this.setState({open: false});};
 
     render() {
@@ -30,11 +42,13 @@ export default class LeftNavUndockedExample extends React.Component {
                     onTouchTap={this.handleToggle}
                 />
                 <LeftNav
+                    style={style.leftNav}
                     docked={true}
                     width={200}
                     open={this.state.open}
                     onRequestChange={open => this.setState({open})}
                 >
+                    <Paper style={style.paper}zDepth={0}>DMV<br/>Chinese Test</Paper>
                     <MenuItem primaryText="Welcome" leftIcon={<TagFaces />}/>
                     <MenuItem primaryText="Practice" leftIcon={<LibraryBooks />}/>
                     <MenuItem primaryText="Test" leftIcon={<Dvr />}/>
